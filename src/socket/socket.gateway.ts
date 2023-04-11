2a58c0484b5b849dc10ff3413262a9d661bfb674
derech1e <nurkthomas@gmail.com>
2023-04-11 15:29:52 +0200

    Fix bug

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,79 +1,77 @@
 import {
-    ConnectedSocket, OnGatewayConnection,
-    OnGatewayDisconnect,
-    OnGatewayInit,
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
-} from '@nestjs/websockets';
-import {Server, Socket} from "socket.io";
-import {Logger} from "@nestjs/common";
-import {SocketService} from "./socket.service";
+  ConnectedSocket, OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { Logger } from "@nestjs/common";
+import { SocketService } from "./socket.service";
 
 @WebSocketGateway({
-    cors: {
-        origin: '*',
-    },
-    namespace: 'sonar',
+  cors: {
+    origin: "*"
+  },
+  namespace: "sonar"
 })
 export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
-    constructor(private readonly socketService: SocketService) {}
+  @WebSocketServer()
+  server: Server;
+  ROOM_NAME = "time";
+  userCount = 0;
+  MEASURE_TIME = 100;
+  interValId: any = null;
+  private logger: Logger = new Logger("ChatGateway");
 
-    @WebSocketServer()
-    server: Server;
-    ROOM_NAME = 'time';
-    userCount = 0;
-    MEASURE_TIME = 100;
-    interValId: any = null;
-    private logger: Logger = new Logger('ChatGateway');
+  constructor(private readonly socketService: SocketService) {
+  }
 
-    startInterval = () => {
-        this.interValId = setInterval(async () => {
-            this.server.in(this.ROOM_NAME).emit('time', await this.socketService.measureDistance(this.MEASURE_TIME));
-        }, this.MEASURE_TIME);
+  startInterval = () => {
+    this.interValId = setInterval(async () => {
+      this.server.in(this.ROOM_NAME).emit("time", await this.socketService.measureDistance(this.MEASURE_TIME));
+    }, this.MEASURE_TIME);
+  };
+  checkForInterval = () => {
+    if (this.userCount > 0 && this.interValId == null) {
+      this.startInterval();
+      this.logger.log("Started Interval!");
+    } else if (this.userCount <= 0) {
+      this.stopInterval();
+      this.logger.log("Stopped Interval!");
     }
-    checkForInterval = () => {
-        if(this.userCount > 0) {
-            if(this.interValId == null) {
-                this.startInterval();
-                this.logger.log("Started Interval!")
-            }
-            this.logger.warn("Unexpected Interval >>Start<< Request!")
-        } else {
-            if(this.interValId != null) {
-                this.stopInterval();
-                this.logger.log("Stopped Interval!")
-            }
-            this.logger.warn("Unexpected Interval >>Stop<< Request!")
-        }
-    }
+  };
 
-    stopInterval = () => {
-        clearInterval(this.interValId);
-        this.interValId = null;
-    }
+  stopInterval = () => {
+    clearInterval(this.interValId);
+    this.interValId = null;
+  };
 
-    handleConnection(@ConnectedSocket() client: Socket) {
-        client.join('time');
-        this.userCount++;
-        this.checkForInterval();
-    }
+  handleConnection(@ConnectedSocket() client: Socket) {
+    client.join("time");
+    this.userCount++;
+    this.checkForInterval();
+  }
 
-    handleDisconnect(@ConnectedSocket() client: any): any {
-        client.leave('time');
-        this.userCount--;
-        this.checkForInterval();
-    }
+  handleDisconnect(@ConnectedSocket() client: any): any {
+    client.leave("time");
+    this.userCount--;
+    this.checkForInterval();
+  }
 
-    afterInit(server: any) {
-        this.logger.log("Sensor Ready!");
-    }
+  afterInit(server: any) {
+    this.logger.log("Sensor Ready!");
+  }
 
-    @SubscribeMessage('rejoin')
-    handleMessage(client: Socket) {
-        client.leave("time")
-        this.server.emit("time", "Requested Rejoin!")
-        client.join("time")
-    }
+  @SubscribeMessage("rejoin")
+  handleMessage(client: Socket) {
+    client.leave("time");
+    this.userCount--;
+    this.server.emit("time", "Requested Rejoin!");
+    client.join("time");
+    this.userCount++;
+    this.checkForInterval();
+  }
 }
