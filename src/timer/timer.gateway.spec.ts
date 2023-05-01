87f12def31c0b272908e5cb65642e636625eb769
Thomas Nürk <nurkthomas@gmail.com>
2023-05-01 17:31:12 +0200

    Fix timer gateway test

--- src/timer/timer.gateway.spec.ts
+++ src/timer/timer.gateway.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TimerGateway } from './timer.gateway';
 import { TimerService } from './timer.service';
+import { PlugService } from "../plug/plug.service";
+import { HttpModule } from "@nestjs/axios";
 
 describe('TimerGateway', () => {
   let gateway: TimerGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TimerGateway, TimerService],
+      providers: [TimerGateway, TimerService, PlugService],
+      imports: [HttpModule],
     }).compile();
 
     gateway = module.get<TimerGateway>(TimerGateway);
